b9cbb9fa175ae9d78c3e6229431ccd9d201df538
Thomas Sauvage <thomas.sauvage@polytechnique.edu>
2024-02-02 15:45:07 +0100

    Not working pure_pursuit

--- f1tenth_perso/scripts/pure_pursuit.py
+++ f1tenth_perso/scripts/pure_pursuit.py
@@ -13,11 +13,39 @@ from nav_msgs.msg import Odometry
 
 
 DRIVE_TOPIC = "/nav"  # "/vesc/ackermann_cmd_mux/input/navigation"
+WAYPOINTS_FILENAME = "/home/thomas/rcws/logs/wp-2024-02-02-13-27-15.csv"
 
-DIST_L = 2  # m
+
+NBR_WAYPOINTS = 400
+
+DIST_L = 1  # m
+SMOOTH_ANGLE = 0.5
+MAX_SPEED = 4  # m/s
+
+CURV_OVERHEAD = 5  # in indexes
 
 
-def get_marker_msg(x: float, y: float):
+def get_waypoints() -> np.ndarray:
+    """Import waypoints"""
+
+    with open(WAYPOINTS_FILENAME, "r") as f:
+        waypoints_csv = f.readlines()
+
+    waypoints = np.zeros((NBR_WAYPOINTS, 4))
+
+    nbr_waypoints_per_line = len(waypoints_csv) // NBR_WAYPOINTS
+    for i in range(NBR_WAYPOINTS):
+        line = waypoints_csv[i * nbr_waypoints_per_line]
+        waypoints[i] = line.split(",")
+
+    return waypoints
+
+
+# Structure of WAYPOINTS: x, y, ?, speed
+WAYPOINTS = get_waypoints()
+
+
+def get_marker_msg(x: float, y: float) -> Marker:
     """Return a Marker Message with the given position"""
 
     SCALE = 0.2
@@ -47,7 +75,7 @@ def get_marker_msg(x: float, y: float):
     return marker_msg
 
 
-def get_marker_array_waypoint():
+def get_marker_array_waypoint() -> MarkerArray:
     """Return a Marker Message with the given position"""
 
     SCALE = 0.1
@@ -84,61 +112,19 @@ def get_marker_array_waypoint():
     return marker_array
 
 
-def get_waypoints():
-    """Import waypoints"""
-
-    with open("/home/thomas/rcws/logs/wp-2024-01-26-16-05-00.csv", "r") as f:
-        waypoints_csv = f.readlines()
-
-    waypoints = np.zeros((len(waypoints_csv), 4))
-
-    for i, line in enumerate(waypoints_csv):
-        waypoints[i] = line.split(",")
-
-    return waypoints
-
-
-def get_target_point(x: float, y: float):
-    """Get the target point for the pure pursuit algorithm.
-
-    Args:
-        x (float): The x position of the car.
-        y (float): The y position of the car.
-
-    Returns:
-        np.ndarray: The target point.
-    """
-
-    # Find the farthest waypoint of dist <= DIST_L and the closest waypoint of dist > DIST_L
-    dists_to_car = np.linalg.norm(WAYPOINTS[:, :2] - np.array([x, y]), axis=1)
-
-    values_near = dists_to_car.copy()
-    values_far = dists_to_car.copy()
-
-    values_near[values_near > DIST_L] = -np.inf
-    values_far[values_far <= DIST_L] = np.inf
-
-    closest_waypoint_far = np.argmin(
-        values_far
-    )  # The closest waypoint that is outside the circle of radius L
-
-    return WAYPOINTS[closest_waypoint_far, :2]
-
-    farthest_waypoint_near = np.argmax(
-        values_near
-    )  # The farthest waypoint that is in the circle of radius L
-
-    # Best point is the mean of the two
-    target_point = (
-        WAYPOINTS[farthest_waypoint_near, :2] + WAYPOINTS[closest_waypoint_far, :2]
-    ) / 2
-
-    return target_point
-
-
-# Structure of WAYPOINTS: x, y, ?, speed
-WAYPOINTS = get_waypoints()
 WAYPOINTS_MARKER = get_marker_array_waypoint()
+
+
+def get_nav_msg(angle: float, speed: float) -> AckermannDriveStamped:
+    """Return an AckermannDriveStamped Message with the given steering angle"""
+
+    drive_msg = AckermannDriveStamped()
+    drive_msg.header.stamp = rospy.Time.now()
+    drive_msg.header.frame_id = "base_link"
+    drive_msg.drive.speed = speed
+    drive_msg.drive.steering_angle = angle
+
+    return drive_msg
 
 
 class PurePursuit(object):
@@ -161,11 +147,9 @@ class PurePursuit(object):
             "/dynamic_viz_array", MarkerArray, queue_size=10
         )
 
-        self.marker_array_pub.publish(get_marker_array_waypoint())
-
         rospy.Subscriber("/odom", Odometry, self.pose_callback)
 
-    def from_global_to_car_frame(self, x: float, y: float):
+    def from_global_to_car_frame(self, x: float, y: float) -> np.ndarray:
         """Transform a point from the global frame to the car frame.
 
         Args:
@@ -188,9 +172,69 @@ class PurePursuit(object):
         transformer = self.tf_buffer.lookup_transform("base_link", "map", rospy.Time())
         point_transformed = tf2_geometry_msgs.do_transform_pose(point, transformer)
 
-        return point_transformed.pose.position.x, point_transformed.pose.position.y
+        return np.array(
+            [point_transformed.pose.position.x, point_transformed.pose.position.y]
+        )
 
-    def pose_callback(self, pose_msg: Odometry):
+    def get_target_point(self, x: float, y: float) -> "tuple[int, np.ndarray]":
+        """Get the target point for the pure pursuit algorithm.
+
+        Args:
+            x (float): The x position of the car.
+            y (float): The y position of the car.
+
+        Returns:
+            int: The index of the target point in the car frame.
+            np.ndarray: The target point.
+        """
+
+        # Find the farthest waypoint of dist <= DIST_L and the closest waypoint of dist > DIST_L
+        dists_to_car = np.linalg.norm(WAYPOINTS[:, :2] - np.array([x, y]), axis=1)
+        values_far = dists_to_car.copy()
+        values_far[values_far <= DIST_L] = np.inf
+
+        while True:
+            closest_waypoint_far = np.argmin(
+                values_far
+            )  # The closest waypoint that is outside the circle of radius L
+
+            target_point = WAYPOINTS[closest_waypoint_far, :2]
+            target_point_car_frame = self.from_global_to_car_frame(
+                target_point[0], target_point[1]
+            )
+
+            # If the target point is in front of the car, return it
+            if np.dot(target_point_car_frame, np.array([1, 0])) >= 0:  # type: ignore
+                return closest_waypoint_far, target_point_car_frame
+            else:
+                print(" [INFO]: Target point behind the car, removing it from the list")
+                values_far[closest_waypoint_far] = np.inf
+
+    def get_speed(self, target_point_car_frame_index: int) -> float:
+        """Return the speed of the car.
+
+        Args:
+            target_point_car_frame (np.ndarray): The target point in the car frame.
+
+        Returns:
+            float: The speed of the car.
+        """
+
+        target = WAYPOINTS[target_point_car_frame_index, :2]
+
+        print((np.linalg.norm(target), target_point_car_frame_index))
+
+        target_overhead = WAYPOINTS[
+            (target_point_car_frame_index + CURV_OVERHEAD) % NBR_WAYPOINTS, :2
+        ]
+
+        waypoints_curv = np.dot(target, target_overhead) / (
+            np.linalg.norm(target) * np.linalg.norm(target_overhead)
+        )
+
+        return waypoints_curv * MAX_SPEED
+
+    def pose_callback(self, pose_msg: Odometry) -> None:
         if self.starting_pos_x is None or self.starting_pos_y is None:
             self.starting_pos_x = pose_msg.pose.pose.position.x
             self.starting_pos_y = pose_msg.pose.pose.position.y
@@ -199,26 +243,26 @@ class PurePursuit(object):
         x = pose_msg.pose.pose.position.x - self.starting_pos_x
         y = pose_msg.pose.pose.position.y - self.starting_pos_y
 
-        target_point = get_target_point(x, y)
+        print(pose_msg.pose.pose.position.x, self.starting_pos_x, x)
 
-        target_point_car_frame = self.from_global_to_car_frame(
-            target_point[0], target_point[1]
+        target_point_car_frame_index, target_point_car_frame = self.get_target_point(
+            x, y
         )
 
         self.marker_pub.publish(
             get_marker_msg(target_point_car_frame[0], target_point_car_frame[1])
         )
 
+        # Calculate curvature/steering angle
+        curvature = 2 * target_point_car_frame[1] / DIST_L**2
+
+        # publish drive message, don't forget to limit the steering angle between -0.4189 and 0.4189 radians
+        speed = self.get_speed(target_point_car_frame_index)
+        self.drive_pub.publish(get_nav_msg(curvature * SMOOTH_ANGLE, speed))
+
         # self.marker_array_pub.publish(WAYPOINTS_MARKER)
-
-        # TODO: transform goal point to vehicle frame of reference
-
-        # TODO: calculate curvature/steering angle
-
-        # TODO: publish drive message, don't forget to limit the steering angle between -0.4189 and 0.4189 radians
-
         print(
-            f"CAR {x:.2f} {y:.2f} | T {target_point[0]:.2f} {target_point[1]:.2f} | T_F {target_point_car_frame[0]:.2f} {target_point_car_frame[1]:.2f}"
+            f"CAR {x:.2f} {y:.2f} | TFC {target_point_car_frame[0]:.2f} {target_point_car_frame[1]:.2f} | CURV {curvature:.2f} | SPEED {speed:.2f}"
         )
 
 
